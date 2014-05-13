ac6cb66cb23ddcbb586e57552c747cb57a4e7d60
Matthew Vona <mzvona@dslextreme.com>
2014-05-12 21:01:31 -0700

    Assignment 2 Code
    Here, including the digest library, I compute the md5 checksum of the
    invertible matrix passed to the cacheSolve() function.
    cacheSolve()
    If the md5 doesn’t exist in the present environment, then the original
    matrix hasn’t been solved, and makeCacheMatrix is called. If it does
    exist, the solved matrix is returned.
    makeCacheMatrix()
    Create a variable to store the solution using md5 of the original
    matrix. Solve the matrix (assumes it is invertible), store the solution
    to the variable, and display the solution.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+library(digest)
 
-makeCacheMatrix <- function(x = matrix()) {
+
+##This function creates a special "matrix" object that can cache its inverse.
+cacheSolve <- function(x = matrix, envir= -1) {
+      ## Generate the md5 of the input matrix to determine the name of the
+      ## variable which should stores it's inverse
+      CHKSUM<-digest(x,algo="md5")  
+      #If the existing array has previously been solved, return the result
+      #which is already stored in the parent environment under the name of the resultant md5
+        if(exists(CHKSUM,envir=-1)){ 
+          
+            #Tell the user it's already been solved, and display the value
+            message("Is already here")
+            get(CHKSUM, envir= -1)
+        #If it hasn't been solved before, solve it by calling makeCacheMatrix display the value 
+        } else {
+            message("Wasn't here, better solve for it")
+            makeCacheMatrix(x)
+        }
+}
+makeCacheMatrix <- function(x = matrix, envir= -1,...) {
+      ## Generate the md5 of the parent matrix to name the variable which stores
+      ## it's inverse
+      CHKSUM<-digest(x,algo="md5")
+      ## Return a matrix that is the inverse of 'x'
+      ## Per assignment, assuming always invertible
+      result<-solve(x)
+      ##Assign the inverted matrix value to the variable named
+      ##by the MD5 of the parent matrix
+      assign(CHKSUM, result, inherits=TRUE)
+      ##Display the solution
+      get(CHKSUM, envir= -1)
 
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
